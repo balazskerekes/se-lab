9b34651c00242c1c68aae31ad1b998a50cb08147
Kerekes Balázs <kerekesbalazs05@gmail.com>
2025-10-07 22:42:04 +0200

    added command for lab purposes

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -39,6 +39,7 @@ public class TorpedoStore {
 
     // simulate random overheating of the launcher bay which prevents firing
     double r = random.nextDouble();
+    //Random method fixed here
 
     if (r >= FAILURE_RATE) {
       // successful firing
